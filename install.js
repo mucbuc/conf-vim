b1be74aa76eb28841258c81c10c8688e833d32d1
mucbuc <ubuntu@ip-172-31-20-251.us-west-2.compute.internal>
2019-02-26 16:59:11 +0000

    pseudo install

---
+++ install.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log('check for existing config');
+console.log('copy and rename configs to home folder');
+console.log('for .bashrc source p.bashrc if .bashrc already exists');
+
+	
+	
+	
