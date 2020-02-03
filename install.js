04e4c6bf5fd67939d6c529bf250ec6244d622f38
mucbuc <mbusenitz@gmail.com>
2020-02-02 21:44:56 -0800

    cleanup

--- install.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-console.log('check for existing config');
-console.log('copy and rename configs to home folder');
-console.log('for .bashrc source p.bashrc if .bashrc already exists');
-
-	
-	
-	
